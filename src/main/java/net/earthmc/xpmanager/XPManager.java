459f693ae5ae8829db8c1b16cf7b2fe81abc42c9
Fruitloopins <49851457+Fruitloopins@users.noreply.github.com>
2023-11-16 12:31:11 +1100

    Refactor main class

--- src/main/java/net/earthmc/xpmanager/XPManager.java
+++ src/main/java/net/earthmc/xpmanager/XPManager.java
@@ -5,6 +5,7 @@ import net.earthmc.xpmanager.command.BottleCommand;
 import net.earthmc.xpmanager.listener.ExpBottleListener;
 import net.earthmc.xpmanager.listener.ProjectileLaunchListener;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class XPManager extends JavaPlugin {
@@ -14,11 +15,8 @@ public final class XPManager extends JavaPlugin {
     public void onEnable() {
         INSTANCE = this;
 
-        getCommand("bottle").setExecutor(new BottleCommand());
-        getCommand("bottleadmin").setExecutor(new BottleAdminCommand());
-
-        getServer().getPluginManager().registerEvents(new ExpBottleListener(), this);
-        getServer().getPluginManager().registerEvents(new ProjectileLaunchListener(), this);
+        initCommands();
+        initListeners();
 
         getLogger().info("XPManager enabled");
     }
@@ -26,5 +24,17 @@ public final class XPManager extends JavaPlugin {
     @Override
     public void onDisable() {
         getLogger().info("XPManager disabled");
+    }
+
+    private void initCommands() {
+        getCommand("bottle").setExecutor(new BottleCommand());
+        getCommand("bottleadmin").setExecutor(new BottleAdminCommand());
+    }
+
+    private void initListeners() {
+        PluginManager pm = getServer().getPluginManager();
+
+        pm.registerEvents(new ExpBottleListener(), this);
+        pm.registerEvents(new ProjectileLaunchListener(), this);
     }
 }
