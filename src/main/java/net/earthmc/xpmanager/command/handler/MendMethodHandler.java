c776c0a398360396bcc3264c9ae6f088449a1f3a
Fruitloopins <49851457+Fruitloopins@users.noreply.github.com>
2024-04-24 02:02:14 +1000

    Add feedback to mend method

--- src/main/java/net/earthmc/xpmanager/command/handler/MendMethodHandler.java
+++ src/main/java/net/earthmc/xpmanager/command/handler/MendMethodHandler.java
@@ -2,6 +2,7 @@ package net.earthmc.xpmanager.command.handler;
 
 import net.earthmc.xpmanager.api.XPManagerMessaging;
 import net.earthmc.xpmanager.object.MethodHandler;
+import net.earthmc.xpmanager.util.BottleUtil;
 import net.earthmc.xpmanager.util.ExperienceUtil;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -30,12 +31,13 @@ public class MendMethodHandler extends MethodHandler {
             return;
         }
 
-        int amount = damage / 2;
+        int amount = (damage / 2) + 1; // Add one extra to get the last bit
         if (!(ExperienceUtil.getTotalXP(player) >= amount)) {
             XPManagerMessaging.sendErrorMessage(player, "You do not have enough experience");
             return;
         }
 
         StoreMethodHandler.givePlayerStoreBottleQuantity(player, amount, 1);
+        XPManagerMessaging.sendSuccessMessage(player, "Successfully stored " + BottleUtil.getPrettyNumber(amount) + " XP");
     }
 }
